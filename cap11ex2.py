624cd3a78855cb9be699abab3e24a73aaaa9e8e1
samucancld <samuel5848@gmail.com>
2022-06-09 13:46:44 -0300

    commenting

--- cap11ex2.py
+++ cap11ex2.py
@@ -9,27 +9,43 @@
 #Enter file:mbox-short.txt
 #39756
 
+#import 'r'egular 'e'xpressions library
 import re
+#infinite loop
 while True:
+    #prompt for the name
     fname = input('fname> ')
+    #try to open a file with the given name
     try:
-        #print(fname[-4:])
+        #add extension if necessary
         if fname[-4:] == '.txt':
             handler = open(fname)
         else:
             handler = open(fname+'.txt')
+        #if the file have been succesfully open, break the loop
         break
+    #catch the error of open a file that doesnt exist
     except:
         print("file doesn't found")
         continue
-
+#initializing list of valuable values
 list_of_values = list()
+#initializing counter in 0
 c = 0
+#for each line in the file
 for each_line in handler:
+    #remove the \n at the end of the line
     each_line.rstrip()
+    #save the return of re.findall, could be a blank list or a list with the value in str
+    #the regex mean maatch everything that begin with 'New Revision: ' followed by 1 or
+    #more characters that are numbers from 0 to 9, extracting just the numbers
     list_of_1_match = re.findall('^New Revision: ([0-9]+)',each_line)
+    #make sure that at the coincidence is just 1 (cant be more numbers)
     if len(list_of_1_match) != 1:
         continue
+    #append the number to the list of vallues typing it to integer
     list_of_values.append(int(list_of_1_match[0]))
+    #increase the counter
     c = c + 1
+#print the average
 print(int(sum(list_of_values)/c))
